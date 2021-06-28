92188685e34c27efed3589e2df5b09c713d6e863
Hex <hex@codeigniter.org.cn>
2021-06-28 17:01:44 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,125 @@
 # postcss-unit-processor
+
+PostCSS plugin to process css unit.
+
+## Install
+
+```shell
+$ npm install postcss postcss-unit-processor --save-dev
+```
+
+## Usage
+
+Use the processor function provided by the user to process the CSS unit. The default processor function is not to do any processing.
+
+### Input/Output
+
+```css
+// input
+h1 {
+  margin: 0 0 20px;
+  font-size: 32px;
+  line-height: 1.2;
+  letter-spacing: 1px;
+}
+
+// output
+h1 {
+  margin: 0 0 20px;
+  font-size: 32px;
+  line-height: 1.2;
+  letter-spacing: 1px;
+}
+```
+
+### Example
+
+```js
+var fs = require('fs');
+var postcss = require('postcss');
+var unitProcessor = require('postcss-unit-processor');
+var css = fs.readFileSync('main.css', 'utf8');
+var options = {
+  processor: (value, unit) => {
+    if (unit === 'px') {
+      return value / 2;
+    }
+  }
+};
+var processedCss = postcss(unitProcessor(options)).process(css).css;
+
+fs.writeFile('main-new.css', processedCss, function (err) {
+  if (err) {
+    throw err;
+  }
+  console.log('New file written.');
+});
+```
+
+### options
+
+Type: `Object | Null`
+Default:
+```js
+{
+  processor: (value) => value,
+  unitPrecision: 5,
+  propList: ['*'],
+  selectorBlackList: [],
+  replace: true,
+  mediaQuery: false,
+  exclude: /node_modules/i
+}
+```
+
+- `unitPrecision` (Number) The decimal numbers to allow the processed units to grow to.
+- `propList` (Array) The properties that can be changed by the processor function.
+    - Values need to be exact matches.
+    - Use wildcard `*` to enable all properties. Example: `['*']`
+    - Use `*` at the start or end of a word. (`['*position*']` will match `background-position-y`)
+    - Use `!` to not match a property. Example: `['*', '!letter-spacing']`
+    - Combine the "not" prefix with the other prefixes. Example: `['*', '!font*']`
+- `selectorBlackList` (Array) The selectors to ignore.
+    - If value is string, it checks to see if selector contains the string.
+        - `['body']` will match `.body-class`
+    - If value is regexp, it checks to see if the selector matches the regexp.
+        - `[/^body$/]` will match `body` but not `.body`
+- `replace` (Boolean) Replace rules instead of adding fallbacks.
+- `mediaQuery` (Boolean) Allow processor function in media queries.
+- `exclude` (String, Regexp, Function) The file path to ignore.
+    - If value is string, it checks to see if file path contains the string.
+        - `'exclude'` will match `\project\postcss-unit-processor\exclude\path`
+    - If value is regexp, it checks to see if file path matches the regexp.
+        - `/exclude/i` will match `\project\postcss-unit-processor\exclude\path`
+    - If value is function, you can use exclude function to return a true and the file will be ignored.
+        - the callback will pass the file path as  a parameter, it should returns a Boolean result.
+        - `function (file) { return file.indexOf('exclude') !== -1; }`
+
+### Use with gulp-postcss and autoprefixer
+
+```js
+var gulp = require('gulp');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var unitProcessor = require('postcss-unit-processor');
+
+gulp.task('css', function () {
+
+  var processors = [
+    autoprefixer({
+      browsers: 'last 1 version'
+    }),
+    unitProcessor({
+      processor: (value, unit) => {
+        if (unit === 'px') {
+          return value / 2;
+        }
+      }
+    })
+  ];
+
+  return gulp.src(['build/css/**/*.css'])
+    .pipe(postcss(processors))
+    .pipe(gulp.dest('build/css'));
+});
+```
---
+++ index.js
@@ -0,0 +1,222 @@
+const unitRegex = /"[^"]+"|'[^']+'|url\([^)]+\)|var\([^)]+\)|(\d*\.?\d+)(px|pt|pc|cm|mm|in|%|em|rem|ch|vh|vw|vmin|vmax|ex)/g;
+
+const filterPropList = {
+  exact: list => list.filter(m => m.match(/^[^*!]+$/)),
+  contain: list =>
+    list.filter(m => m.match(/^\*.+\*$/)).map(m => m.substr(1, m.length - 2)),
+  endWith: list => list.filter(m => m.match(/^\*[^*]+$/)).map(m => m.substr(1)),
+  startWith: list =>
+    list.filter(m => m.match(/^[^*!]+\*$/)).map(m => m.substr(0, m.length - 1)),
+  notExact: list =>
+    list.filter(m => m.match(/^![^*].*$/)).map(m => m.substr(1)),
+  notContain: list =>
+    list.filter(m => m.match(/^!\*.+\*$/)).map(m => m.substr(2, m.length - 3)),
+  notEndWith: list =>
+    list.filter(m => m.match(/^!\*[^*]+$/)).map(m => m.substr(2)),
+  notStartWith: list =>
+    list.filter(m => m.match(/^![^*]+\*$/)).map(m => m.substr(1, m.length - 2))
+};
+
+const typeToString = s =>
+  Object.prototype.toString
+    .call(s)
+    .slice(8, -1)
+    .toLowerCase();
+
+const types = [
+  "String",
+  "Array",
+  "Undefined",
+  "Boolean",
+  "Number",
+  "Function",
+  "Symbol",
+  "Object"
+];
+
+const type = types.reduce((acc, str) => {
+  acc["is" + str] = val => typeToString(val) === str.toLowerCase();
+  return acc;
+}, {});
+
+const defaults = {
+  processor: (value) => value,
+  unitPrecision: 5,
+  selectorBlackList: [],
+  propList: ['*'],
+  replace: true,
+  mediaQuery: false,
+  exclude: /node_modules/i
+};
+
+function createUnitReplace(processor, unitPrecision, root) {
+  return (node) => (m, $1, $2) => {
+    if (!$1) {
+      return m;
+    }
+
+    const value = parseFloat($1);
+    const unit = $2;
+    const result = processor(value, unit, node, root);
+
+    let newValue, newUnit;
+    if (type.isObject(result) && result !== null) {
+      newValue = parseFloat(result.value) || 0;
+      newUnit = result.unit || unit;
+    }
+    else if (type.isNumber(result) || type.isString(result)) {
+      newValue = parseFloat(result) || 0;
+      newUnit = unit;
+    }
+    else {
+      newValue = value;
+      newUnit = unit;
+    }
+
+    const fixedVal = toFixed(newValue, unitPrecision);
+
+    return fixedVal === 0 ? "0" : fixedVal + newUnit;
+  };
+}
+
+function toFixed(number, precision) {
+  const multiplier = Math.pow(10, precision + 1);
+  const wholeNumber = Math.floor(number * multiplier);
+
+  return (Math.round(wholeNumber / 10) * 10) / multiplier;
+}
+
+function blacklistedSelector(blacklist, selector) {
+  if (typeof selector !== "string") {
+    return;
+  }
+
+  return blacklist.some(regex => {
+    if (typeof regex === "string") {
+      return selector.indexOf(regex) !== -1;
+    }
+
+    return selector.match(regex);
+  });
+}
+
+function createPropListMatcher(propList) {
+  const hasWild = propList.indexOf("*") > -1;
+  const matchAll = hasWild && propList.length === 1;
+  const lists = {
+    exact: filterPropList.exact(propList),
+    contain: filterPropList.contain(propList),
+    startWith: filterPropList.startWith(propList),
+    endWith: filterPropList.endWith(propList),
+    notExact: filterPropList.notExact(propList),
+    notContain: filterPropList.notContain(propList),
+    notStartWith: filterPropList.notStartWith(propList),
+    notEndWith: filterPropList.notEndWith(propList)
+  };
+
+  return prop => {
+    if (matchAll) {
+      return true;
+    }
+
+    return (
+      (hasWild ||
+        lists.exact.indexOf(prop) > -1 ||
+        lists.contain.some(function(m) {
+          return prop.indexOf(m) > -1;
+        }) ||
+        lists.startWith.some(function(m) {
+          return prop.indexOf(m) === 0;
+        }) ||
+        lists.endWith.some(function(m) {
+          return prop.indexOf(m) === prop.length - m.length;
+        })) &&
+      !(
+        lists.notExact.indexOf(prop) > -1 ||
+        lists.notContain.some(function(m) {
+          return prop.indexOf(m) > -1;
+        }) ||
+        lists.notStartWith.some(function(m) {
+          return prop.indexOf(m) === 0;
+        }) ||
+        lists.notEndWith.some(function(m) {
+          return prop.indexOf(m) === prop.length - m.length;
+        })
+      )
+    );
+  };
+}
+
+module.exports = (options = {}) => {
+  const opts = Object.assign({}, defaults, options);
+  const satisfyPropList = createPropListMatcher(opts.propList);
+  const exclude = opts.exclude;
+  let isExcludeFile = false;
+  let unitReplace;
+
+  return {
+    postcssPlugin: "postcss-unit-processor",
+
+    Once(css) {
+      const filePath = css.source.input.file;
+
+      if (
+        exclude &&
+        ((type.isFunction(exclude) && exclude(filePath)) ||
+          (type.isString(exclude) && filePath.indexOf(exclude) !== -1) ||
+          filePath.match(exclude) !== null)
+      ) {
+        isExcludeFile = true;
+      } else {
+        isExcludeFile = false;
+      }
+
+      unitReplace = createUnitReplace(opts.processor, opts.unitPrecision, css);
+    },
+
+    Declaration(decl) {
+      if (isExcludeFile) {
+        return;
+      }
+
+      if (
+        !unitRegex.test(decl.value) ||
+        !satisfyPropList(decl.prop) ||
+        blacklistedSelector(opts.selectorBlackList, decl.parent.selector)
+      ) {
+        return;
+      }
+
+      const value = decl.value.replace(unitRegex, unitReplace(decl));
+
+      // if unit already processed, do not add or replace
+      if (decl.__unitProcessorFinished === true) {
+        return;
+      }
+
+      if (opts.replace) {
+        decl.__unitProcessorFinished = true;
+        decl.value = value;
+      }
+      else {
+        decl.cloneAfter({ value, __unitProcessorFinished: true });
+      }
+    },
+
+    AtRule(atRule) {
+      if (isExcludeFile) {
+        return;
+      }
+
+      if (opts.mediaQuery && atRule.name === "media") {
+        if (atRule.__unitProcessorFinished === true || !unitRegex.test(atRule.params)) {
+          return;
+        }
+        atRule.__unitProcessorFinished = true;
+        atRule.params = atRule.params.replace(unitRegex, unitReplace(atRule));
+      }
+    }
+  };
+};
+
+module.exports.postcss = true;
